8b5c11ffcb4ddb2b49489a0ec59082938d0e578d
resteve <resteve@zikzakmedia.com>
2015-12-01 10:47:02 +0100

    Test Scenerario

--- tests/test_account_bank_statement.py
+++ tests/test_account_bank_statement.py
@@ -2,8 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountBankStatementTestCase(ModuleTestCase):
@@ -15,4 +17,11 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountBankStatementTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_bank_statement.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_bank_statement_different_currency.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
