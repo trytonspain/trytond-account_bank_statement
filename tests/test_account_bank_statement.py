236f04feed682a0bfc76a860da376a70f3e850f2
resteve <resteve@zikzakmedia.com>
2015-12-01 10:45:50 +0100

    Tests

--- tests/test_account_bank_statement.py
+++ tests/test_account_bank_statement.py
@@ -1,43 +1,18 @@
-#!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of the account_bank_statement module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountBankStatementTestCase(unittest.TestCase):
-    '''
-    Test AccountBankStatement module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_bank_statement')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('account_bank_statement')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class AccountBankStatementTestCase(ModuleTestCase):
+    'Test Account Bank Statement module'
+    module = 'account_bank_statement'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountBankStatementTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_bank_statement.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_bank_statement_different_currency.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
