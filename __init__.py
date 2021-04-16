ddfcc978808d32875a6c731dd3c8d6778790495e
Jared Esparza <jared.esparza@nan-tic.com>
2021-04-16 11:45:58 +0200

    Remove account bank reconciliation

--- __init__.py
+++ __init__.py
@@ -3,9 +3,6 @@
 
 from trytond.pool import Pool
 from . import statement
-from . import move
-from . import reconciliation
-from . import account
 from . import journal
 
 
@@ -15,14 +12,7 @@ def register():
         statement.Statement,
         statement.StatementLine,
         statement.ImportStart,
-        reconciliation.AccountBankReconciliation,
-        move.Line,
-        move.Move,
-        account.AccountTemplate,
-        account.Account,
-        move.OpenBankReconcileLinesStart,
         module='account_bank_statement', type_='model')
     Pool.register(
         statement.Import,
-        move.OpenBankReconcileLines,
         module='account_bank_statement', type_='wizard')
